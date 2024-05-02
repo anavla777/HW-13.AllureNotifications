5c6968b3c90f7807263a14c9c7ff01e148077bad
anavla777 <w.ananenkow@gmail.com>
2024-05-02 19:46:01 +0300

    Поправил локатор выбора дня из календаря

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -7,6 +7,6 @@ public class CalendarComponent {
         public void setDate(String day, String month, String year) {
             $(".react-datepicker__month-select").selectOption(month);
             $(".react-datepicker__year-select").selectOption(year);
-            $(String.format(".react-datepicker__day--0%s", day)).click();
+            $(String.format(".react-datepicker__day--0%s:not(.react-datepicker__day--outside-month)", day)).click();
         }
 }
